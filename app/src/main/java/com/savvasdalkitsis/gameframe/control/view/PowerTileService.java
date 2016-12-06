9d04342c4a50caa5e3c17430ee5bed33244d12c7
Savvas Dalkitsis <savvas.dalkitsis@gmail.com>
2016-12-06 21:17:22 +0000

    Quick setting tile to toggle power on N devices

---
+++ app/src/main/java/com/savvasdalkitsis/gameframe/control/view/PowerTileService.java
@@ -0,0 +1,24 @@
+package com.savvasdalkitsis.gameframe.control.view;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.service.quicksettings.TileService;
+
+import com.savvasdalkitsis.gameframe.rx.RxTransformers;
+import com.savvasdalkitsis.gameframe.usecase.GameFrameUseCase;
+
+import static com.savvasdalkitsis.gameframe.injector.usecase.UseCaseInjector.gameFrameUseCase;
+
+@TargetApi(Build.VERSION_CODES.N)
+public class PowerTileService extends TileService {
+
+    private final GameFrameUseCase gameFrameUseCase = gameFrameUseCase();
+
+    @Override
+    public void onClick() {
+        super.onClick();
+        gameFrameUseCase.togglePower()
+                .compose(RxTransformers.schedulers())
+                .subscribe(n->{}, e->{});
+    }
+}
