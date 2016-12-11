c9ac3169f72a317975c1aba3726c345a2d253045
Savvas Dalkitsis <savvas.dalkitsis@gmail.com>
2016-12-11 14:14:55 +0000

    Fixing problem with network calls failing every other time

--- app/src/main/java/com/savvasdalkitsis/gameframe/gameframe/api/GameFrameApi.java
+++ app/src/main/java/com/savvasdalkitsis/gameframe/gameframe/api/GameFrameApi.java
@@ -5,6 +5,8 @@ import java.util.Map;
 import retrofit2.http.FieldMap;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import retrofit2.http.QueryMap;
 import rx.Observable;
@@ -13,8 +15,10 @@ public interface GameFrameApi {
 
     @FormUrlEncoded
     @POST("command")
+    @Headers("Connection: close")
     Observable<Void> command(@FieldMap(encoded = true) Map<String, String> fields);
 
     @GET("set")
+    @Headers("Connection: close")
     Observable<Void> set(@QueryMap() Map<String, String> params);
 }
