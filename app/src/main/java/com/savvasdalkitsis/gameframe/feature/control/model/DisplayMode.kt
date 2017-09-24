e7bdd11da5b587b0f51d5504a96b954456b008a1
Savvas Dalkitsis <savvas.dalkitsis@gmail.com>
2017-09-24 18:09:27 +0100

    Adding support for the new 'effects' display mode

--- app/src/main/java/com/savvasdalkitsis/gameframe/feature/control/model/DisplayMode.kt
+++ app/src/main/java/com/savvasdalkitsis/gameframe/feature/control/model/DisplayMode.kt
@@ -3,13 +3,14 @@ package com.savvasdalkitsis.gameframe.feature.control.model
 enum class DisplayMode(val queryParamName: String, private val level: Int) {
 
     GALLERY("m0", 0),
-    CLOCK("m1", 1);
+    CLOCK("m1", 1),
+    EFFECTS("m2", 2);
 
     companion object {
 
         fun from(level: Int): DisplayMode {
             return values().firstOrNull { it.level == level }
-                    ?: if (level < 0) GALLERY else CLOCK
+                    ?: if (level < 0) GALLERY else EFFECTS
         }
     }
 }
